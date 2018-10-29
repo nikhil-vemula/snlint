2b8f651cecffc17bc9b0a5d55c05a059b669a531
srinivasa.vemula <srinivasa.vemula@servicenow.com>
2018-10-29 12:26:55 +0530

    Modified

--- setup.sh
+++ setup.sh
@@ -20,36 +20,20 @@ echo "
 }
 
 install() {
-        workingDir=$(pwd)
-        #Find the git folders and install pre-commit
-        echo "Installing snlint in $gitDir"
-        # Ignore config file in git commit
-        echo ".pre-commit-config.yaml" >> ~/.gitignore_global
-        git config --global core.excludesfile ~/.gitignore_global
-        for dir in $(find $gitDir -name ".git")
-        do
-            cd $dir
-            cd ..
-            echo "Copying configuration file to $(pwd)"
-            cp ~/snlint/.pre-commit-config.yaml .pre-commit-config.yaml
-            git rm --cached ./.pre-commit-config.yaml > /dev/null 2>&1
-            pre-commit install
-            pre-commit install --hook-type pre-push
-            pre-commit install --hook-type commit-msg
-        done
-        if [ ! -f /usr/local/bin/snlint ]; then
-            ln -s ~/snlint/setup.sh /usr/local/bin/snlint
-        fi;
-        cd $workingDir
+    echo "Installing snlint..."
+    # Npm install
+    cd ~/snlint
+    npm install .
+    # Modify git hook default directory.
+    git config --global core.hooksPath ~/snlint/snhooks
+    # Create soft link
+    if [ ! -f /usr/local/bin/snlint ]; then
+        ln -s ~/snlint/setup.sh /usr/local/bin/snlint
+    fi;
 }
 update() {
-    echo "Updating snlint at $gitDir..."
-    for file in $(find ~/git -name ".pre-commit-config.yaml")
-    do
-        cd $(dirname $file)
-        echo "Updating the pre-commit hooks...."
-        pre-commit autoupdate
-    done
+    echo "Updating snlint..."
+    npm install
 }
 checkForUpdates() {
     git remote update
@@ -71,9 +55,6 @@ checkForUpdates() {
     fi
 }
 
-gitDir=$(pwd)
-echo $gitDir
-#Changing the default directory
 cd ~
 
 #Print Info
@@ -88,23 +69,14 @@ elif [ -z "$1" ] || $1 = "install" ] || [ $1 = "update" ]; then
         cd snlint
         echo "Checking for updates....";
         checkForUpdates;
-        install;
         update;
     fi;
 #Uninstall
 elif [ $1 = "uninstall" ]; then
-    echo "Uninstalling snlint from $gitDir...."
-    for file in $(find ~/git -name ".pre-commit-config.yaml")
-    do
-        cd $(dirname $file)
-        echo "Uninstalling the pre-commit hooks...."
-        pre-commit uninstall
-        pre-commit uninstall --hook-type pre-push
-        pre-commit uninstall --hook-type commit-msg
-        echo "Deleting config file $file"
-        rm $file
-    done
+    echo "Uninstalling snlint..."
+    git config --global --unset core.hooksPath
     rm /usr/local/bin/snlint
+    rm -rf ~/snlint
 fi;
 
 
